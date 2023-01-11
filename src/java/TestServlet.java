cdf3f6a50afa69be96efe72e805aeb279b82b5f1
PorthoGamesBR <84247243+PorthoGamesBR@users.noreply.github.com>
2023-01-11 18:27:05 -0300

    Formulario envia dados para servlet

--- src/java/TestServlet.java
+++ src/java/TestServlet.java
@@ -22,6 +22,7 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String c = request.getParameter("celsius");
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -31,7 +32,7 @@ public class TestServlet extends HttpServlet {
             out.println("<title>Servlet TestServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet TestServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Graus em Celsius: " + c + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
